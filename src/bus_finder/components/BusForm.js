3e33ca0e29cc05c4df8526b7b64ffce138e8911f
Leandro Rodriguez <lrodriguezjb@gmail.com>
2020-02-11 12:37:40 -0800

    appdesign

--- src/bus_finder/components/BusForm.js
+++ src/bus_finder/components/BusForm.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
-import { TextInput, Text, Button, ShadowPropTypesIOS, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+    TextInput,
+    Text,
+    Button,
+    ShadowPropTypesIOS,
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Image,
+    Dimensions
+} from "react-native";
 import { render } from "react-dom";
-import BusMap from './BusMap.js'
+import BusMap from "./BusMap.js";
+const { width } = Dimensions.get("screen");
 
 export default function BusForm(props) {
-
     const busState = {
         busNumber: "",
 
@@ -13,22 +23,18 @@ export default function BusForm(props) {
             closest_direction: null,
             closest_minutes: null,
             closest_lat: null,
-            closest_lon: null,
+            closest_lon: null
         },
         nextClosestData: {
             next_closest_name: null,
             next_closest_direction: null,
             next_closest_minutes: null,
             next_closest_lat: null,
-            next_closest_lon: null,
-        },
-
-
-
+            next_closest_lon: null
+        }
     };
-    const [mapDisplay, setMapDisplay] = React.useState(false)
+    const [mapDisplay, setMapDisplay] = React.useState(false);
     const [busData, updateBusData] = React.useState(busState);
-
 
     function submitHandler(event) {
         let url = `http://178.128.6.148:8000/api/v1/${props.lat}/${props.long}/${busData.busNumber}`;
@@ -43,78 +49,97 @@ export default function BusForm(props) {
         //         console.error(error);
         //     });
 
-        setMapDisplay(true)
+        setMapDisplay(true);
     }
 
-    function returnHome(){
-        setMapDisplay(false)
+    function returnHome() {
+        setMapDisplay(false);
     }
-    
+
     let homeButton;
     let button;
     let busmap;
 
     if (mapDisplay) {
-        busmap = <BusMap lat={props.lat} long={props.long} closest={busData.closestData} nextClosest={busData.nextClosestData} />
-        button = <></>
-        homeButton= <Button title='back' onPress={() => returnHome()} > </Button>
-    } else {
-        busmap = <></>
-        button = <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                onChangeText={text => updateBusData({ busNumber: text })}
-                value={busData.busNumber}
+        busmap = (
+            <BusMap
+                lat={props.lat}
+                long={props.long}
+                closest={busData.closestData}
+                nextClosest={busData.nextClosestData}
             />
-            <TouchableOpacity
-                style={styles.submitButton}
-                onPress={() => submitHandler()}>
-                <Text style={styles.submitButtonText}> Where's My Bus </Text>
-            </TouchableOpacity>
-
-        </View>
+        );
+        button = <></>;
+        homeButton = (
+            <Button title="back" onPress={() => returnHome()}>
+                {" "}
+            </Button>
+        );
+    } else {
+        busmap = <></>;
+        button = (
+            <View style={styles.container}>
+                <Text style={styles.header}>Where's My Bus?</Text>
+                <TouchableOpacity
+                    style={styles.submitButton}
+                    onPress={() => submitHandler()}
+                >
+                    <Image
+                        style={styles.submitButton}
+                        source={require("./button.png")}
+                    />
+                </TouchableOpacity>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={text => updateBusData({ busNumber: text })}
+                    value={busData.busNumber}
+                />
+            </View>
+        );
     }
 
-
-
     return (
-        <View>
+        <View style={styles.container}>
             {homeButton}
             {button}
             {busmap}
-
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    header: {
+        color: "white",
+        justifyContent: "center", //Centered vertically
+        alignItems: "center", // Centered horizontally,
+        fontWeight: "bold",
+        fontSize: 50,
+        paddingBottom: 70
+    },
     container: {
-        paddingTop: 23,
-
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#54123B",
+        paddingTop: 40,
+        ...StyleSheet.absoluteFillObject
     },
     input: {
-        margin: 15,
+        width: width / 2,
+        margin: 80,
         height: 40,
-        borderColor: '#7a42f4',
+        borderColor: "#7A42F4",
         borderWidth: 1,
-        textAlign: 'center',
-
+        textAlign: "center"
     },
     submitButton: {
-        backgroundColor: '#7a42f4',
+        alignItems: "center",
         padding: 10,
-        margin: 15,
-        height: 40,
-        width: 150,
-
-
+        width: width / 1.5,
+        height: width / 1.5
     },
     submitButtonText: {
-        color: 'white'
+        color: "white"
     }
-
-
 });
 // when button Submit clicked > call event handler, that will make an API call to back end
 // if call was successsful render Details component
